e53768a8c9844df799878d191d2f3a78bc594b0f
Vlado Tesanovic <Vlado.Tesanovic@cad-q.com>
2015-09-30 09:45:40 +0200

    Added lot of events on page.ts

--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,22 +1,25 @@
 /// <reference path="../../../typings/angular2/angular2.d.ts" />
 
-import { Component, View } from "angular2/angular2";
+import { Component, View, LifecycleEvent, NgFor } from "angular2/angular2";
 import { RouteParams } from "angular2/router";
 import { SEMANTIC_COMPONENTS } from "../../directives/semantic/semantic";
 import { PageServices } from "../../services/page";
 
 @Component({
 	bindings: [PageServices],
-	selector: "page"
+	lifecycle: [LifecycleEvent.OnDestroy, LifecycleEvent.OnChanges, LifecycleEvent.OnInit,
+		LifecycleEvent.AfterContentInit],
+		selector: "page"
 })
 @View({
-	directives: [SEMANTIC_COMPONENTS],
+	directives: [SEMANTIC_COMPONENTS, NgFor],
 	templateUrl: "./templates/components/page/page.html"
 })
 
 export class PageComponent {
 	public text: string;
 	public title: string;
+	public events: Array<string> = new Array<string>();
 
 	constructor(params: RouteParams, pageServ: PageServices) {
 		pageServ.getPage().subscribe(res => {
@@ -27,5 +30,20 @@ export class PageComponent {
 				}
 			});
 		});
+	}
+	onChange(value: string) {
+		this.text = value;
+	}
+	onInit() {
+		this.events.push("onInit");
+	}
+	onChanges() {
+		this.events.push("onChanges");
+	}
+	afterContentInit() {
+		this.events.push("afterContentInit");
+	}
+	onDestroy() {
+		this.events.push("onDestroy");
 	}
 }
