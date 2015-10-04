23b2d44731139a5e3ff06cd291ee2a3b6d50a8c2
Vlado Tesanovic <Vlado.Tesanovic@cad-q.com>
2015-10-05 00:58:23 +0200

    added travis config file

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,10 +1,9 @@
 /// <reference path="../typings/angular2/angular2.d.ts" />
 
 import { bootstrap, bind } from "angular2/angular2";
-import { ROUTER_BINDINGS } from "angular2/router";
+import { ROUTER_BINDINGS, LocationStrategy, HashLocationStrategy } from "angular2/router";
 import { HTTP_BINDINGS } from "angular2/http";
 
-import { LocationStrategy, HashLocationStrategy } from "angular2/router";
 import { AppComponent } from "./components/app";
 
 bootstrap(AppComponent, [
