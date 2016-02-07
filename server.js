a9cc38883e44a4d1f96b4498b2e505ca3a92c0cc
Vlado Tesanovic <vlado.tesanovic@gmail.com>
2016-02-07 18:55:12 +0100

    static-files and finalhandler removed

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 var http = require('http');
+var fs = require('fs');
 
-var finalhandler = require('finalhandler');
-var serveStatic = require('serve-static');
-
-var serve = serveStatic("./demo");
-
-var server = http.createServer(function(req, res) {
-  var done = finalhandler(req, res);
-  serve(req, res, done);
-});
-
-server.listen(process.env.PORT || 3000, function() {
-  console.log("Server started");
-});
+http.createServer(function (req, res) {
+  if(req.url.length < 2) {
+    req.url = "/index.html";
+  }
+  fs.readFile(__dirname + "/demo" + req.url, function (err,data) {
+    res.writeHead(200);
+    res.end(data);
+  });
+}).listen(3000);
