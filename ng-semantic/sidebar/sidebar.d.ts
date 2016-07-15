d54158c35f6c9ad997ffb9fb2fb7c7ad19db2c5d
Vlado Tesanovic <vlado.tesanovic@gmail.com>
2016-07-15 20:39:23 +0200

    bundle

--- ng-semantic/sidebar/sidebar.d.ts
+++ ng-semantic/sidebar/sidebar.d.ts
@@ -1,6 +1,6 @@
 import { ElementRef, OnInit, OnDestroy, Renderer } from "@angular/core";
 export declare class SemanticSidebarComponent implements OnInit, OnDestroy {
-    private renderer;
+    renderer: Renderer;
     class: string;
     sidebar: ElementRef;
     private visible;
