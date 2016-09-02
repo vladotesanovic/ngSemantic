5657bf631327110eaf733ab286e9be2979a33fc2
Frank Adrian <fad@facelift-bbt.com>
2016-09-02 13:41:25 +0200

    Point to UMD bundles in SystemJS configuration

--- systemjs.config.js
+++ systemjs.config.js
@@ -4,13 +4,41 @@
  */
 (function(global) {
     // map tells the System loader where to look for things
+   var paths = {
+       'npm': 'node_modules/'
+   };
+
     var map = {
         'app':                        'demo/app', // 'dist',
-        '@angular':                   'node_modules/@angular',
-        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-        'rxjs':                       'node_modules/rxjs',
         'ng-semantic':                './',
-        "Prism":                      'demo/vendor/prismjs'
+        'Prism':                      'demo/vendor/prismjs',
+
+
+        // angular bundles
+        '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+        '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+        '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+        '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+        '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+        '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+        '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+        '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+
+        // angular testing umd bundles
+        '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
+        '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
+        '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
+        '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
+        '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
+        '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
+        '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
+        '@angular/forms/testing': 'npm:@angular/forms/bundles/forms-testing.umd.js',
+
+        // other libraries
+        'rxjs': 'npm:rxjs',
+        'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api'
+
+
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
@@ -45,6 +73,7 @@
     // Add package entries for angular packages
     ngPackageNames.forEach(setPackageConfig);
     var config = {
+        paths: paths,
         map: map,
         packages: packages
     };
