c969ea2a91030e6f90e79026026fd931a61a50e1
Frank Adrian <frankwebdevelopment@gmail.com>
2016-09-03 16:01:20 +0200

    Clean up systemjs config file

--- systemjs.config.js
+++ systemjs.config.js
@@ -2,16 +2,14 @@
  * System configuration for Angular 2 samples
  * Adjust as necessary for your application needs.
  */
-(function(global) {
+(function (global) {
     // map tells the System loader where to look for things
-   var paths = {
-       'npm': 'node_modules/'
-   };
+    var paths = {
+        'npm:': 'node_modules/'
+    };
 
     var map = {
-        'app':                        'demo/app', // 'dist',
-        'ng-semantic':                './',
-        'Prism':                      'demo/vendor/prismjs',
+        'app': 'demo/app', // 'dist',
 
 
         // angular bundles
@@ -24,54 +22,37 @@
         '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
         '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
 
-        // angular testing umd bundles
-        '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
-        '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
-        '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
-        '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
-        '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
-        '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
-        '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
-        '@angular/forms/testing': 'npm:@angular/forms/bundles/forms-testing.umd.js',
-
         // other libraries
         'rxjs': 'npm:rxjs',
-        'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api'
+        'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
+        'ng-semantic': './',
+        'Prism': 'demo/vendor/prismjs',
 
 
     };
-    // packages tells the System loader how to load when no filename and/or no extension
+
     var packages = {
-        'app':                        { main: 'bootstrap.js',  defaultExtension: 'js' },
-        'rxjs':                       { defaultExtension: 'js' },
-        'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
-        'ng-semantic':                { main: 'ng-semantic.js', defaultExtension: 'js' },
-        'Prism':                      { main: 'prism.js', defaultExtension: 'js' }
+        app: {
+            main: './bootstrap.js',
+            defaultExtension: 'js'
+        },
+        rxjs: {
+            defaultExtension: 'js'
+        },
+        'angular2-in-memory-web-api': {
+            main: './index.js',
+            defaultExtension: 'js'
+        },
+        'ng-semantic': {
+            main: './ng-semantic.js',
+            defaultExtension: 'js'
+        },
+        'Prism': {
+            main: './prism.js',
+            defaultExtension: 'js'
+        }
     };
-    var ngPackageNames = [
-        'common',
-        'compiler',
-        'core',
-        'http',
-        'forms',
-        'platform-browser',
-        'platform-browser-dynamic',
-        'router',
-        'router-deprecated',
-        'upgrade'
-    ];
-    // Individual files (~300 requests):
-    function packIndex(pkgName) {
-        packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
-    }
-    // Bundled (~40 requests):
-    function packUmd(pkgName) {
-        packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
-    }
-    // Most environments should use UMD; some (Karma) need the individual index files
-    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
-    // Add package entries for angular packages
-    ngPackageNames.forEach(setPackageConfig);
+
     var config = {
         paths: paths,
         map: map,
