682aa645003cfc4bb2135e2eb2180c632fba2df7
Vlado Tesanovic <vlado.tesanovic@gmail.com>
2016-09-04 10:48:24 +0200

    Removed npm run typings from postinstall

--- ng-semantic/shape/shape.d.ts
+++ ng-semantic/shape/shape.d.ts
@@ -1,5 +1,5 @@
 import { ElementRef } from "@angular/core";
 export declare class SemanticShapeComponent {
     shape: ElementRef;
-    show(options?: {}): void;
+    show(...args: string[]): void;
 }
