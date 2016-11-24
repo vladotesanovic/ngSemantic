4c5eacb4b343ee261569a2e18290d29821d51c88
Steven Guiheux <steven.guiheux@corp.ovh.com>
2016-11-24 10:00:27 +0100

    feat (select): add flag to disabled dropdown
    Signed-off-by: Steven Guiheux <steven.guiheux@corp.ovh.com>

--- ng-semantic/select/select.d.ts
+++ ng-semantic/select/select.d.ts
@@ -4,6 +4,7 @@ export declare class SemanticSelectComponent implements AfterViewInit {
     control: FormControl;
     class: string;
     label: string;
+    disabled: boolean;
     options: {};
     placeholder: string;
     modelChange: EventEmitter<string | number>;
