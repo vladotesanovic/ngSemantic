85bda07f74c4b672c4fde6f7745fdf3e94476657
andrej-vasilj <andrej_vasilj@hotmail.com>
2017-03-27 10:36:58 -0400

    Update for Semantic 2.2.9
    Added the possibility of a custom arrow icon.
    Added a named hidden input that automatically updates value onChange.

--- src/dropdown/dropdown.ts
+++ src/dropdown/dropdown.ts
@@ -8,10 +8,11 @@ declare var jQuery: any;
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: "sm-dropdown",
   template: `
-    <div class="ui {{class}} dropdown item" #dropdown>
+    <div class="ui {{class}} dropdown" #dropdown>
+        <input type="hidden" name="{{name}}" #input>
         <i *ngIf="icon" class="{{icon}} icon"></i>
-        <div *ngIf="title" class="text">{{title}}</div>
-        <i class="dropdown icon"></i>
+        <div *ngIf="title" class="default text">{{title}}</div>
+        <i class="dropdown icon {{arrowIcon}}"></i>
         <div class="menu">
             <ng-content></ng-content>
         </div>
@@ -22,16 +23,20 @@ export class SemanticDropdownComponent implements AfterViewInit {
   @Input() class: string;
   @Input() title: string;
   @Input() icon: string;
+  @Input() arrowIcon: string;
+  @Input() name: string;
   @Input() items: Array<{}>;
   @Input() options: {} = {};
   @Output() onChange: EventEmitter<string|number> = new EventEmitter<string|number>();
   @ViewChild("dropdown") dropdown: ElementRef;
+  @ViewChild("input") input: ElementRef;
 
   ngAfterViewInit(): void {
 
     const options: {} = Object.assign({
       onChange: (value: string|number, a: string|number, b: Array<HTMLElement>) => {
         if (b != null && b.length) {
+          this.input.nativeElement.value = b[0].dataset.value;
           this.onChange.emit(b[0].innerText);
         }
       }
