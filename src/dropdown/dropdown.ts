16c4e67796ee157dc460b4db24189d83c5a02146
Steven Guiheux <steven.guiheux@corp.ovh.com>
2017-05-26 11:16:47 +0200

    fix: disabled dropdown like sm-select

--- src/dropdown/dropdown.ts
+++ src/dropdown/dropdown.ts
@@ -24,6 +24,18 @@ export class SemanticDropdownComponent implements AfterViewInit {
   @Input() icon: string;
   @Input() items: Array<{}>;
   @Input() options: {} = {};
+
+  @Input("disabled")
+  set disabled(data: boolean) {
+    setTimeout(() => {
+      if (data) {
+        jQuery(this.dropdown.nativeElement).addClass("disabled");
+      } else {
+        jQuery(this.dropdown.nativeElement).removeClass("disabled");
+      }
+    }, 1);
+  };
+
   @Output() onChange: EventEmitter<string|number> = new EventEmitter<string|number>();
   @ViewChild("dropdown") dropdown: ElementRef;
 
