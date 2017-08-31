f8d92e3dd113de78a6ed9e5aeb3bd6bb7cb86360
Parlee, Michael <michael.parlee@thermofisher.com>
2017-08-31 11:44:36 -0700

    Support for error appearance in progress component. Allows user to pass in an error check function and applies error style if true.

--- demo/app/components/elements/progress.ts
+++ demo/app/components/elements/progress.ts
@@ -1,35 +1,40 @@
 import { Component } from "@angular/core";
+import {ErrorCheckFunction} from "../../../../src/progress/progress";
 
 @Component({
     selector: "sm-page-progress",
     template: `
-	<div class="ui masthead vertical segment">
-    <div class="ui container">
-        <h1>Progress</h1>
-        <p>Semantic UI Progress module <i class="icon external"></i>
-        <a href="http://semantic-ui.com/modules/progress.html" target="_blank">Semantic UI Progress</a></p>
-    </div>
-</div>
-<div class="main ui container">
-    <p>Simple progress bar</p>
-    <h4 class="ui header">Demo</h4>
-    <sm-progress [progress]="percent"></sm-progress>
-    <sm-progress class="warning" [progress]="percent"></sm-progress>
-    
-    <h4 class="ui header">Code</h4>
-    <sm-codeblock smPrismjs="html">
-&lt;sm-progress [progress]="percent">&lt;/sm-progress>
-&lt;sm-progress class="warning" [progress]="percent">&lt;/sm-progress>
-    </sm-codeblock>
-    <div class="ui horizontal section icon divider"><i class="icon setting"></i></div>
-    <h4 class="ui header">Demo indicating</h4>
-    <sm-progress class="indicating active" [progress]="indicatingPercent"></sm-progress>
-    <sm-codeblock smPrismjs="html">
-&lt;sm-progress class="indicating active" [progress]="indicatingPercent">&lt;/sm-progress>
-    </sm-codeblock>
-    <div class="ui horizontal section icon divider"><i class="icon setting"></i></div>
-</div>
-`
+      <div class="ui masthead vertical segment">
+        <div class="ui container">
+          <h1>Progress</h1>
+          <p>Semantic UI Progress module <i class="icon external"></i>
+            <a href="http://semantic-ui.com/modules/progress.html" target="_blank">Semantic UI Progress</a></p>
+        </div>
+      </div>
+      <div class="main ui container">
+        <p>Simple progress bar</p>
+        <h4 class="ui header">Demo</h4>
+        <sm-progress [progress]="percent"></sm-progress>
+        <sm-progress class="warning" [progress]="percent"></sm-progress>
+        <sm-progress class="blue" [progress]="percent" [errorCheckFn]="findErrorAt50Percent"></sm-progress>
+
+        <h4 class="ui header">Code</h4>
+        <sm-codeblock smPrismjs="html">
+          &lt;sm-progress [progress]="percent">&lt;/sm-progress>
+          &lt;sm-progress class="warning" [progress]="percent">&lt;/sm-progress>
+          &lt;sm-progress class="blue" [progress]="percent" [errorCheckFn]="findErrorAt50Percent"&gt;&lt;/sm-progress&gt;
+        </sm-codeblock>
+        <div class="ui horizontal section icon divider"><i class="icon setting"></i></div>
+        <h4 class="ui header">Demo indicating</h4>
+        <sm-progress class="indicating active" [progress]="indicatingPercent"></sm-progress>
+        <sm-codeblock smPrismjs="html">
+          &lt;sm-progress class="indicating active" [progress]="indicatingPercent">&lt;/sm-progress>
+        </sm-codeblock>
+        <div class="ui horizontal section icon divider"><i class="icon setting"></i></div>
+        <sm-codeblock smPrismjs="html">
+        </sm-codeblock>
+      </div>
+    `
 })
 
 export class ProgressComponent {
@@ -40,4 +45,8 @@ export class ProgressComponent {
         setInterval(() => this.percent += 10, 1000);
         setInterval(() => this.indicatingPercent += 4, 1000);
     }
+
+    findErrorAt50Percent: ErrorCheckFunction = (): boolean => {
+      return this.percent > 50;
+    };
 }
